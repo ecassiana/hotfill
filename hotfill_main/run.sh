71ddef9dae97b877ae93a7469ba0e0e909640692
Elis Cassiana <elisan@alunos.utfpr.edu.br>
2021-12-23 17:58:13 -0300

    updating .sh files

---
+++ hotfill_main/run.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+clear
+
+if [[ ! -e ./tests/out/ ]]; then
+    mkdir ./tests/out/
+fi
+
+./script_gcode_solve.sh RP3 rp3
+
+./script_gcode_solve.sh SLIC3R slic3r
+
+
+./script_scanline_solve.sh SCANLINE
+
+./script_scanline_solve.sh ALTSCANLINE
+
+
+./script_hotfill.sh
+
+
+python3 ./summary_results_main.py False
--- hotfill_main/script_hotfill.sh
+++ hotfill_main/script_hotfill.sh
@@ -2,85 +2,85 @@
 
 DELTA=1.4
 for LIMIT in 5 15 20 25 30 80; do
-    ./_script_hotfill_solve.sh ${DELTA} ${LIMIT} 
+    ./script_hotfill_solve.sh ${DELTA} ${LIMIT} 
 done
 
 DELTA=2.0
 for LIMIT in 5 15 20 25 30 80; do
-    ./_script_hotfill_solve.sh ${DELTA} ${LIMIT} 
+    ./script_hotfill_solve.sh ${DELTA} ${LIMIT} 
 done
 
 DELTA=2.3
 for LIMIT in 20; do
-    ./_script_hotfill_solve.sh ${DELTA} ${LIMIT} 
+    ./script_hotfill_solve.sh ${DELTA} ${LIMIT} 
 done
 
 DELTA=2.8
 for LIMIT in 5 15 20 25 30 80; do
-    ./_script_hotfill_solve.sh ${DELTA} ${LIMIT} 
+    ./script_hotfill_solve.sh ${DELTA} ${LIMIT} 
 done
 
 DELTA=4.0
 for LIMIT in 5 15 20 25 30 80; do
-    ./_script_hotfill_solve.sh ${DELTA} ${LIMIT} 
+    ./script_hotfill_solve.sh ${DELTA} ${LIMIT} 
 done
 
 DELTA=5.7
 for LIMIT in 5 15 20 25 30 80; do
-    ./_script_hotfill_solve.sh ${DELTA} ${LIMIT} 
+    ./script_hotfill_solve.sh ${DELTA} ${LIMIT} 
 done
 
 DELTA=8.0
 for LIMIT in 5 10 15 20 25 30 40 50 60 70 80; do
-    ./_script_hotfill_solve.sh ${DELTA} ${LIMIT} 
+    ./script_hotfill_solve.sh ${DELTA} ${LIMIT} 
 done
 
 DELTA=11.3
 for LIMIT in 5 15 20 25 30 80; do
-    ./_script_hotfill_solve.sh ${DELTA} ${LIMIT} 
+    ./script_hotfill_solve.sh ${DELTA} ${LIMIT} 
 done
 
 DELTA=16.0
 for LIMIT in 5 10 15 20 25 30 40 50 60 70 80; do
-    ./_script_hotfill_solve.sh ${DELTA} ${LIMIT} 
+    ./script_hotfill_solve.sh ${DELTA} ${LIMIT} 
 done
 
 DELTA=22.6
 for LIMIT in 5 15 20 25 30 80; do
-    ./_script_hotfill_solve.sh ${DELTA} ${LIMIT} 
+    ./script_hotfill_solve.sh ${DELTA} ${LIMIT} 
 done
 
 DELTA=32.0
 for LIMIT in 5 15 20 25 30 80; do
-    ./_script_hotfill_solve.sh ${DELTA} ${LIMIT} 
+    ./script_hotfill_solve.sh ${DELTA} ${LIMIT} 
 done
 
 DELTA=45.3
 for LIMIT in 5 15 20 25 30 80; do
-    ./_script_hotfill_solve.sh ${DELTA} ${LIMIT} 
+    ./script_hotfill_solve.sh ${DELTA} ${LIMIT} 
 done
 
 DELTA=64.0
 for LIMIT in 5 10 15 20 25 30 40 50 60 70 80; do
-    ./_script_hotfill_solve.sh ${DELTA} ${LIMIT} 
+    ./script_hotfill_solve.sh ${DELTA} ${LIMIT} 
 done
 
 DELTA=90.5
 for LIMIT in 5 15 20 25 30 80; do
-    ./_script_hotfill_solve.sh ${DELTA} ${LIMIT} 
+    ./script_hotfill_solve.sh ${DELTA} ${LIMIT} 
 done
 
 DELTA=128.0
 for LIMIT in 5 15 20 25 30 80; do
-    ./_script_hotfill_solve.sh ${DELTA} ${LIMIT} 
+    ./script_hotfill_solve.sh ${DELTA} ${LIMIT} 
 done
 
 DELTA=181.0
 for LIMIT in 5 15 20 25 30 80; do
-    ./_script_hotfill_solve.sh ${DELTA} ${LIMIT} 
+    ./script_hotfill_solve.sh ${DELTA} ${LIMIT} 
 done
 
 DELTA=256.0
 for LIMIT in 5 15 20 25 30 80; do
-    ./_script_hotfill_solve.sh ${DELTA} ${LIMIT} 
+    ./script_hotfill_solve.sh ${DELTA} ${LIMIT} 
 done
